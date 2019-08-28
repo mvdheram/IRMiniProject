889f962c49e36e5f81a9651f55b470627af051b9
Meher Vivek <vivekdheram@gmail.com>
2019-08-29 01:01:47 +0200

    Adding search with querybuilder

--- src/main/java/search.java
+++ src/main/java/search.java
@@ -1,7 +1,3 @@
-import org.aksw.qa.commons.datastructure.IQuestion;
-import org.aksw.qa.commons.load.Dataset;
-import org.aksw.qa.commons.load.LoaderController;
-import org.aksw.qa.commons.measure.AnswerBasedEvaluation;
 import org.apache.http.HttpHost;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.CharArraySet;
@@ -14,26 +10,39 @@ import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.*;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class search {
     String tsv = "C:/Users/Meher/Desktop/IR/wiki.tsv";
+    public static RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+    public String url="garbage value";
+    public String text="garbage value";
+
 
     public static void main(String args[])
     {
         search search = new search();
-        List<IQuestion> questions = LoaderController.load(Dataset.QALD7_Train_Multilingual);
+       // List<IQuestion> questions = LoaderController.load(Dataset.QALD7_Train_Multilingual);
         try {
             search.index();
-            search.measure(questions);
+            search.Query(preprocessing("When was the Battle of Gettysburg?"),"date");
+
+            //search.measure(questions);
+
         }
         catch (Exception e)
         {
@@ -65,12 +74,8 @@ public class search {
 
     public void index() throws Exception{
         try {
-            RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
             System.out.println("start indexing");
-
-
-            String url = null;
-            String text = null;
             int i = 1;
 
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(tsv))));
@@ -85,7 +90,6 @@ public class search {
                     url = line.split("\t")[0];
                     text = line.split("\t")[1];
                 }
-
                 IndexRequest indexRequest = new IndexRequest("wiki").id(Integer.toString(i)).source("url", url, "text",  preprocessing(text));
                 GetRequest getRequest = new GetRequest("wiki", Integer.toString(i));
                 try {
@@ -111,22 +115,46 @@ public class search {
 
     }
 
-   public static Set<String> Query(String text, String answerType)
+   public Set<String> Query(String searchText, String answerType) throws java.io.IOException
    {
        Set<String> systemAnswers = new HashSet();
-       return null;
+       SearchRequest searchRequest = new SearchRequest();
+       SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+       QueryBuilder matchQuery = QueryBuilders.matchQuery("text",searchText);
+       searchSourceBuilder.query(matchQuery);
+       searchRequest.source(searchSourceBuilder);
+
+       SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+       SearchHits hits = searchResponse.getHits();
+
+       System.out.println("Found " + hits.getTotalHits() + " hits.");
+
+       SearchHit[] searchHits = hits.getHits();
+       for (SearchHit hit : searchHits) {
+           String sourceAsString = hit.getSourceAsString();
+           float score = hit.getScore();
+           System.out.println("result of search"+sourceAsString);
+           System.out.println("score"+score);
+       }
+
+
+       return systemAnswers;
 
    }
     
-    public void measure(List<IQuestion> questions) throws Exception
-    {
-
-        for (IQuestion question : questions)
-        {
-            AnswerBasedEvaluation.fMeasure(Query(preprocessing(question.getLanguageToQuestion().get("en")), question.getAnswerType()),question);
-        }
-
-
-    }
+//    public void measure(List<IQuestion> questions) throws Exception
+//    {
+//
+//        for (IQuestion question : questions)
+//        {
+//            System.out.println("measure");
+//            //AnswerBasedEvaluation.fMeasure(Query(preprocessing(question.getLanguageToQuestion().get("en")), question.getAnswerType()),question);
+//            String preprocessd = preprocessing(question.getLanguageToQuestion().get("en"));
+//            Query(preprocessd,question.getAnswerType());
+//        }
+//
+//
+//    }
 
 }
